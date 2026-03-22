0c5d1763ab6f16c1d47a00eed329c005bca9904b
AustinDeng <nidhogg.dxx@gmail.com>
2026-03-23 02:20:03 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,9 @@ pnpm run docs:build
 # 进入生成的文件夹
 cd src/.vuepress/dist
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:AustinDeng/AustinDeng.github.io.git main:gh-pages
+git push -f git@github.com:AustinDeng/AustinDeng.github.io.git master
